4e993985a02d0969b35467daf8c7edfa44906f1d
nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-02-04 08:33:03 +0200

    Add project serializer.

---
+++ projects/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import *
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+        
