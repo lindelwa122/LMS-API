37532b9fc331f2dd19651045d0b466acf5ef78e5
nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-02-04 09:33:03 +0200

    Show project name in admin.

--- projects/models.py
+++ projects/models.py
@@ -4,3 +4,6 @@ class Project(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False, unique=True)
     url = models.CharField(max_length=500, blank=False, null=False, unique=True)
     due_date = models.DateTimeField(blank=False, null=False)
+    
+    def __str__(self):
+        return self.name
