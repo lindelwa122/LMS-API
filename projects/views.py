014a3fb7554b1d6c296be48ec137be2a114de907
nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-02-04 08:38:24 +0200

    Add a view to get a project.

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import *
+from .serializer import *
+
+def get_project(request, name, format=None):
+    project = Project.objects.get(name=name)
+    if not project:
+        return Response({ 'error': 'Project not found.' }, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = ProjectSerializer(project)
+    return Response(serializer.data)
