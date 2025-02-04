aa4fb20474e45422b4b9d445a2cbe01010b20276
nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-02-04 08:26:15 +0200

    Add project model.

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False, unique=True)
+    url = models.CharField(max_length=500, blank=False, null=False, unique=True)
+    due_date = models.DateTimeField(blank=False, null=False)
