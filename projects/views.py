e658b289f977c4d463276b5fa3f181b80c3dcbaf
nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-02-06 15:13:55 +0200

    Add a view to grade students.

--- projects/views.py
+++ projects/views.py
@@ -15,3 +15,30 @@ def get_project(request, name, format=None):
     
     serializer = ProjectSerializer(project)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def update_grade(request, format=None):
+    username = request.data['username']
+    score = request.data['grade']
+    project_id = request.data['project_id']
+    
+    # Try to get project
+    try:
+        project = Project.objects.get(name=project_id)
+    except ObjectDoesNotExist:
+        return Response({ 'error': 'Project not found.' }, status=status.HTTP_404_NOT_FOUND)
+
+    student, _ = Student.objects.get_or_create(username=username)
+        
+    try:
+        grade = Grade.objects.get(student=student, project=project)
+        
+        # Only update grade if the new score is better than the previous one
+        if grade.score < score:
+            grade.objects.update(score=score)
+            grade.save()
+            
+    except ObjectDoesNotExist:
+        grade = Grade.objects.get(student=student, project=project, score=score)
+        grade.score()
