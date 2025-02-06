108494a1ca3a8e58525ba68d86d27d607ff5dc4c
nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-02-06 12:26:10 +0200

    Add student and grade model.

--- projects/models.py
+++ projects/models.py
@@ -7,3 +7,19 @@ class Project(models.Model):
     
     def __str__(self):
         return self.name
+
+
+class Student(models.Model):
+    username = models.CharField(max_length=100, blank=False, null=False, unique=True)
+    
+    def __str__(self):
+        return self.username
+    
+
+class Grade:
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='student')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='project')
+    score = models.FloatField()
+    
+    def __str__(self):
+        return f'{self.student} ({self.project}) = {self.score}'
