8c9523e43738516e57d332e00b42ae29fef30594
nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-02-07 08:14:58 +0200

    Try installing sqlite3 compatible with vercel.

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,7 @@ echo "Building project packages..."
 python3 -m pip install -r requirements.txt
 
 echo "Migrating Database..."
+apt-get update && apt-get install -y sqlite3 libsqlite3-dev
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
