e53d94294bf8a5ef6a14599af79f991456b90d3e
nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-02-07 08:16:19 +0200

    Remove migrations for now.

--- build_files.sh
+++ build_files.sh
@@ -4,9 +4,9 @@ echo "Building project packages..."
 python3 -m pip install -r requirements.txt
 
 echo "Migrating Database..."
-apt-get update && apt-get install -y sqlite3 libsqlite3-dev
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+# apt-get update && apt-get install -y sqlite3 libsqlite3-dev
+# python3 manage.py makemigrations --noinput
+# python3 manage.py migrate --noinput
 
 echo "Collectiong static files..."
 python3 manage.py collectstatic --noinput
